6ca851cd79f4486df0a4032a1a1e99049e3a77b6
DESKTOP-2R2K8U1\Ricardo <richisais3535@gmail.com>
2023-08-23 08:17:07 -0600

    mi segunda vez

---
+++ src/Estructuras/pilas/Pilas.java
@@ -0,0 +1,53 @@
+package Estructuras.pilas;
+
+public class Pilas {
+	private Object[] Pilas;
+	private int size;
+	public Pilas(int size) {
+		this.Pilas=new Object[size];
+		this.size=0;
+	}
+	public void push(Object value) {
+
+		if (Pilas.length<=size)
+		{
+			Pilas[size]= value;
+			size++;
+		}
+	}
+	public Object pop() {
+
+		Object value=null;
+		if(isEmpty())
+		{
+			size--;
+			value=Pilas[size];
+			Pilas[size]=null;
+		}
+		return value;
+	}
+	public  int size() {
+
+		return size;
+	}
+	public  boolean isEmpty() {
+		return size==0;             //Devuelve un boleano 
+	}
+	public  Object peek() {
+		Object value=null;
+		if (size>0)
+		{
+			value=Pilas[size-1];
+		}	
+		return value;
+	}
+	public String toString()
+	{
+		StringBuilder s=new StringBuilder();
+		for(int i=0;i<size;i++)
+		{
+			s.append(Pilas[i]).append("->");
+		}
+		return s.toString();
+	}
+}
---
+++ src/Estructuras/pruebas/Principal.java
@@ -0,0 +1,11 @@
+package Estructuras.pruebas;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+
+	}
+
+}
